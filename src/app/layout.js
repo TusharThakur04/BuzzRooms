31806e4ce8ca95b4ea17695e3dcdd092093392cd
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-06-16 17:31:12 +0530

    feat: add responsive header with navigation and mobile menu toggle

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-import "./globals.css";
+import "./globals.scss";
+import Header from "@/components/header/Header";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState } from "react";
+import { Container } from "../common/container/Container";
+import styles from "./Header.module.scss";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+const Header = () => {
+  const active = false;
+  const location = usePathname();
+  console.log("location", location);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  return (
+    <Container>
+      <header
+        className={`${active ? styles.headerShow : styles.header} flex items-center justify-between`}
+      >
+        <div className={styles.logo}>BuzzRooms</div>
+        <nav
+          className={`${styles.navBar} ${
+            menuOpen ? styles.navVisible : ""
+          } flex items-center justify-between gap-4`}
+        >
+          <div
+            className={`${location === "/" ? styles.active : ""} flex items-center justify-center`}
+          >
+            <Link href="/">Home</Link>
+          </div>
+          <div
+            className={`${location === "/rooms" ? styles.active : ""} flex items-center justify-center`}
+          >
+            <Link href="/rooms">Rooms</Link>
+          </div>
+          <div
+            className={`${location === "/about" ? styles.active : ""} flex items-center justify-center`}
+          >
+            <Link href="/about">About</Link>
+          </div>
+          <button className={styles.close}>
+            <CloseIcon onClick={toggleMenu} />
+          </button>
+        </nav>
+        <div
+          className={styles.auth + " flex items-center justify-center gap-4"}
+        >
+          <button className={styles.login}>Log In</button>
+          <button className={styles.menu} onClick={toggleMenu}>
+            <MenuIcon />
+          </button>
+        </div>
+      </header>
+    </Container>
+  );
+};
+
+export default Header;
