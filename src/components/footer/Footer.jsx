085458a93bcd6c2a2008ad2516a562fd186814c2
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-06-16 18:40:22 +0530

    feat: add and style footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styles from "./Footer.module.scss";
+import Link from "next/link";
+import { Container } from "../common/container/Container";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
-
-export default Footer;
+export default function Footer() {
+  return (
+    <Container>
+      {" "}
+      <footer
+        className={`${styles.footer} flex flex-col md:flex-row items-center justify-between p-6`}
+      >
+        <div className={styles.left}>
+          <div className={styles.logo}>🐝 BuzzRooms</div>
+          <div className={styles.copy}>
+            © {new Date().getFullYear()} BuzzRooms
+          </div>
+        </div>
+        <nav className={`${styles.nav} flex gap-4 mt-4 md:mt-0`}>
+          <Link href="/" className="hover:underline">
+            Home
+          </Link>
+          <Link href="/rooms" className="hover:underline">
+            Rooms
+          </Link>
+          <Link href="/about" className="hover:underline">
+            About
+          </Link>
+        </nav>
+      </footer>
+    </Container>
+  );
+}
