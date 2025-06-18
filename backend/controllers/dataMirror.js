69f57286e598d4086dffb9e32fa6eb649b23765d
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-06-19 01:14:52 +0530

    refactor: prisma client generation and data mirroring

--- backend/controllers/dataMirror.js
+++ backend/controllers/dataMirror.js
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import prisma from "../lib/prisma.js";
 
 export const mirrorUserToDB = async (req, res) => {
   try {
@@ -27,33 +26,3 @@ export const mirrorUserToDB = async (req, res) => {
     return res.status(500).json({ error: "Internal server error" });
   }
 };
-
-// // POST: Mirror user to DB
-// router.post("/", async (req, res) => {
-//   try {
-//     const { clerkId, email, username, image } = req.body;
-
-//     if (!clerkId || !email) {
-//       return res.status(400).json({ error: "Missing required fields" });
-//     }
-
-//     // Upsert: create if doesn't exist, do nothing if already exists
-//     const user = await prisma.user.upsert({
-//       where: { clerkId },
-//       update: {}, // can include updates later if needed
-//       create: {
-//         clerkId,
-//         email,
-//         username,
-//         image,
-//       },
-//     });
-
-//     return res.status(200).json({ message: "User mirrored", user });
-//   } catch (error) {
-//     console.error("Error creating user:", error);
-//     return res.status(500).json({ error: "Internal server error" });
-//   }
-// });
-
-// export default router;
