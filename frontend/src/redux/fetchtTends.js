e3c9a11680ac1e24fb7f3d36d3fbe4d8cfaae6ff
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-06-22 20:51:39 +0530

    refactor: trends fetching logic

--- frontend/src/redux/fetchtTends.js
+++ frontend/src/redux/fetchtTends.js
@@ -8,30 +8,3 @@ export const fetchTrends = async () => {
 export const fetch = {
   fetchTrends,
 };
-
-const showsSlice = createSlice({
-  name: "shows",
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder
-      .addCase(fetchAllShows.pending, (state) => {
-        state.isError.fetchAllShows = false;
-        state.isLoading.fetchAllShows = true;
-        state.isSuccess.fetchAllShows = false;
-      })
-      .addCase(fetchAllShows.rejected, (state, action) => {
-        state.error = action.payload;
-        state.isError.fetchAllShows = true;
-        state.isLoading.fetchAllShows = false;
-        state.isSuccess.fetchAllShows = false;
-      })
-      .addCase(fetchAllShows.fulfilled, (state, { payload }) => {
-        state.isError.fetchAllShows = false;
-        state.isLoading.fetchAllShows = false;
-        state.isSuccess.fetchAllShows = true;
-        state.shows = payload;
-      });
-  },
-});
-export default tredsSLice.reducer;
--- frontend/src/redux/slices/trendsSlice.js
+++ frontend/src/redux/slices/trendsSlice.js
@@ -52,3 +52,30 @@ const initialState = {
   },
   error: null,
 };
+
+const trendsSlice = createSlice({
+  name: "trends",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchAllShows.pending, (state) => {
+        state.isError.fetchAllShows = false;
+        state.isLoading.fetchAllShows = true;
+        state.isSuccess.fetchAllShows = false;
+      })
+      .addCase(fetchAllShows.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isError.fetchAllShows = true;
+        state.isLoading.fetchAllShows = false;
+        state.isSuccess.fetchAllShows = false;
+      })
+      .addCase(fetchAllShows.fulfilled, (state, { payload }) => {
+        state.isError.fetchAllShows = false;
+        state.isLoading.fetchAllShows = false;
+        state.isSuccess.fetchAllShows = true;
+        state.shows = payload;
+      });
+  },
+});
+export default trendsSlice.reducer;
