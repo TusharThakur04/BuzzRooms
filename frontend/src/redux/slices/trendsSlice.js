6e36cc9765864cda8ff5a4aaee155aac6d1b310e
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-06-22 20:43:29 +0530

    feat: add Redux slice and fetch function for trends

---
+++ frontend/src/redux/slices/trendsSlice.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetch } from "../fetchtTends";
+
+const handleError = (error, thunkAPI) => {
+  if (error.response) {
+    const { status, data } = error.response;
+    let code = null;
+    if (status === 404) {
+      code = "404";
+    } else if (status === 429) {
+      code = "429";
+    }
+
+    return thunkAPI.rejectWithValue({
+      code,
+      message: data.message,
+    });
+  } else if (error.request) {
+    return thunkAPI.rejectWithValue({
+      code: error.code,
+      message: "Network Error",
+    });
+  }
+
+  return thunkAPI.rejectWithValue({
+    code: "ERR_GENERIC",
+    message: "Error",
+  });
+};
+
+export const fetchTrends = createAsyncThunk(
+  "fetch/trends",
+  async (_, thunkAPI) => {
+    try {
+      return await fetch.fetchTrends();
+    } catch (error) {
+      return handleError(error, thunkAPI);
+    }
+  }
+);
+
+const initialState = {
+  trends: [],
+  isLoading: {
+    fetchTrends: false,
+  },
+  isSuccess: {
+    fetchTrends: false,
+  },
+  isError: {
+    fetchTrends: false,
+  },
+  error: null,
+};
