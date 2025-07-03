c09cbc4bf92ec0822160783a8152831a910e00e9
Tushar <80577646+TusharThakur04@users.noreply.github.com>
2025-07-04 00:43:32 +0530

    fix: home page loading

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,10 +1,11 @@
 "use client";
-import { useUser } from "@clerk/nextjs";
+import { useUser, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
 import Welcome from "@/components/welcome/Welcome";
 import TopTrends from "@/components/topTrends/TopTrends";
 import Features from "@/components/features/Features";
 import { mirrorUser } from "@/lib/clerk/mirrorData";
 import PrevJoined from "@/components/prevJoined/PrevJoined";
+import { BeatLoader } from "react-spinners";
 
 const page = () => {
   const { isSignedIn, user } = useUser();
@@ -14,11 +15,23 @@ const page = () => {
   }
 
   return (
-    <div>
-      <Welcome isSignedIn={isSignedIn} user={user} />
-      <TopTrends />
-      {isSignedIn ? <PrevJoined /> : <Features />}
-    </div>
+    <>
+      <ClerkLoading>
+        <div
+          style={{ minHeight: "100vh" }}
+          className="flex justify-center align items-center"
+        >
+          <BeatLoader color="#ebe2ec" />
+        </div>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <div>
+          <Welcome isSignedIn={isSignedIn} user={user} />
+          <TopTrends />
+          {isSignedIn ? <PrevJoined /> : <Features />}
+        </div>
+      </ClerkLoaded>
+    </>
   );
 };
 
